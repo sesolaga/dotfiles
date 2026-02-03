f7d66fa8956bda8a66f53321a07d54bc3acaefa5
Sergei Solagaian <solagaian.sergei@yandex.ru>
2026-02-03 21:57:06 +0300

    Style wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,11 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Darcula (base16)"
+-- config.color_scheme = "Darcula (base16)"
+config.color_scheme = "Aura (Gogh)"
+
+config.font = wezterm.font("Dank Mono")
+config.font_size = 14
+config.line_height = 1.4
 
 return config
