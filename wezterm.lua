b647e25a781cc2df66491abcadbb03fa00f75768
Sergei Solagaian <solagaian.sergei@yandex.ru>
2026-02-13 22:55:38 +0300

    Add fallback font + blinking block

--- wezterm.lua
+++ wezterm.lua
@@ -6,8 +6,29 @@ local config = wezterm.config_builder()
 -- config.color_scheme = "Darcula (base16)"
 config.color_scheme = "Aura (Gogh)"
 
-config.font = wezterm.font("Dank Mono")
+config.font = wezterm.font_with_fallback({
+  "Dank Mono",
+  "Symbols Nerd Font",
+})
 config.font_size = 14
 config.line_height = 1.4
+
+-- -- LEADER
+-- config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+--
+-- config.keys = {
+--   -- Activate copy mode (vim-style navigation)
+--   {
+--     key = "[",
+--     mods = "LEADER",
+--     action = wezterm.action.ActivateCopyMode,
+--   },
+-- }
+--
+
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.cursor_blink_rate = 500
 
 return config
